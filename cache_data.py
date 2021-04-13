20dde352dd17e76b653c6d0e926b668e75f628cd
Aren Moosakhanian <mooset_92@hotmail.com>
2021-04-13 11:47:43 +0200

    Dir updated, cache_data.py

--- cache_data.py
+++ cache_data.py
@@ -25,13 +25,13 @@ def cache_masks():
 
 def cache_crops():
   fnpy_root = '/home/aren/repos/STR-PIP/STR-PIP/datasets/JAAD_instance_segm'
-  fpkl_root = '/home/aren/repos/STR-PIP/datasets/cache/JAAD_instance_crops'
+  fpkl_root = '/home/aren/repos/STR-PIP/STR-PIP/datasets/cache/JAAD_instance_crops'
   utils.get_obj_crops(fnpy_root, fpkl_root)
 
 def add_obj_bbox():
-  fnpy_root = '/home/aren/repos/STR-PIP/datasets/JAAD_instance_segm'
+  fnpy_root = '/home/aren/repos/STR-PIP/STR-PIP/datasets/JAAD_instance_segm'
   # fpkl_root = '/sailhome/bingbin/STR-PIP/datasets/cache/jaad_collapse'
-  fobj_root = '/home/aren/repos/STR-PIP/datasets/cache/obj_bbox'
+  fobj_root = '/home/aren/repos/STR-PIP/STR-PIP/datasets/cache/obj_bbox'
   os.makedirs(fobj_root, exist_ok=True)
   dir_vids = sorted(glob(os.path.join(fnpy_root, 'vid*')))
 
@@ -94,7 +94,7 @@ def merge_and_flat(vrange):
   Merge fids in a vid and flatten the classes
   """
   pkl_in_root = '/home/aren/repos/STR-PIP/STR-PIP/datasets/cache/obj_bbox'
-  pkl_out_root = '/home/aren/repos/STR-PIP/datasets/cache/obj_bbox_merged'
+  pkl_out_root = '/home/aren/repos/STR-PIP/STR-PIP/datasets/cache/obj_bbox_merged'
   os.makedirs(pkl_out_root, exist_ok=True)
   # for vid in range(1, 347):
   for vid in vrange:
