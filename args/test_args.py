2ed7eb3787678f60360487ca9aa635742dd9ccae
Aren Moosakhanian <mooset_92@hotmail.com>
2021-04-13 16:57:05 +0200

    Update test_args.py

--- args/test_args.py
+++ args/test_args.py
@@ -15,8 +15,8 @@ class TestArgs(BaseArgs):
     self.parser.add_argument('--batch-size', type=int, default=4, help='batch size')
     self.parser.add_argument('--which-epoch', type=int,
                              help='which epochs to evaluate, -1 to load the best checkpoint')
-    self.parser.add_argument('--slide', type=int, default=1,
-                             help='Whether to use sliding window when testing.')
+    #self.parser.add_argument('--slide', type=int, default=1,
+    #                         help='Whether to use sliding window when testing.')
     self.parser.add_argument('--collect-A', type=int, default=0,
                              help="Whether to collect weight matrices in the graph model.")
     self.parser.add_argument('--save-As-format', type=str, default="",
