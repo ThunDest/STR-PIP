31839264c280df025f149c4a289562d80e778116
Aren Moosakhanian <mooset_92@hotmail.com>
2021-04-13 16:40:31 +0200

    Update test_args.py

--- args/test_args.py
+++ args/test_args.py
@@ -5,11 +5,12 @@ class TestArgs(BaseArgs):
   def __init__(self):
     super(TestArgs, self).__init__()
 
+    print("Point 1.1.1")
     self.is_train = False
     # self.split = 'test'
-
+    print("Point 1.1.2")
     self.parser.add_argument('--mode', type=str, choices=['extract', 'evaluate'])
-
+    print("Point 1.1.3")
     # hyperparameters
     self.parser.add_argument('--batch-size', type=int, default=4, help='batch size')
     self.parser.add_argument('--which-epoch', type=int,
@@ -20,6 +21,7 @@ class TestArgs(BaseArgs):
                              help="Whether to collect weight matrices in the graph model.")
     self.parser.add_argument('--save-As-format', type=str, default="",
                              help="Path to saved weight matrices.")
+    print("Point 1.1.4")
     # self.parser.add_argument('--rand-loader', type=int, default=0,
     #                          help="Whether to randomize the data loader.")
 
