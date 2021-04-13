1ad4d4a5f4a1d92ecc78a293b547d9f0e9503fdf
Aren Moosakhanian <mooset_92@hotmail.com>
2021-04-13 17:03:06 +0200

    Update test_args.py

--- args/test_args.py
+++ args/test_args.py
@@ -8,6 +8,7 @@ class TestArgs(BaseArgs):
     print("Point 1.1.1")
     self.is_train = False
     # self.split = 'test'
+
     print("Point 1.1.2")
     self.parser.add_argument('--mode', type=str, choices=['extract', 'evaluate'])
     print("Point 1.1.3")
@@ -15,8 +16,8 @@ class TestArgs(BaseArgs):
     self.parser.add_argument('--batch-size', type=int, default=4, help='batch size')
     self.parser.add_argument('--which-epoch', type=int,
                              help='which epochs to evaluate, -1 to load the best checkpoint')
-    #self.parser.add_argument('--slide', type=int, default=1,
-    #                         help='Whether to use sliding window when testing.')
+    self.parser.add_argument('--slide', type=str, default=True,
+                             help='Whether to use sliding window when testing.')
     self.parser.add_argument('--collect-A', type=int, default=0,
                              help="Whether to collect weight matrices in the graph model.")
     self.parser.add_argument('--save-As-format', type=str, default="",
